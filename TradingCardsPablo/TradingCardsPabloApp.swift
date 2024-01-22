0e111c58bf6a078ac782fc14af7cb54b34854b70
Russell Gordon <rgordon@lcs.on.ca>
2024-01-22 12:28:07 -0500

    Initial Commit

---
+++ TradingCardsPablo/TradingCardsPabloApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsPabloApp.swift
+//  TradingCardsPablo
+//
+//  Created by Russell Gordon on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsPabloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
